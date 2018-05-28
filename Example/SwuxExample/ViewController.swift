9cdde5970b66b5ff08671fb6100ce142022136a6
Adolfo Rodriguez <adolfo@mokriya.com>
2018-05-28 13:07:29 -0400

    simplified example

--- Example/SwuxExample/ViewController.swift
+++ Example/SwuxExample/ViewController.swift
@@ -8,33 +8,18 @@
 
 import UIKit
 
-private let ballRadius: CGFloat = 20
-
 class ViewController: UIViewController, SubscriberProtocol {
     @IBOutlet weak var canvasView: UIView?
     @IBOutlet weak var jumpButton: UIButton?
-    weak var _ballView: UIView? = nil
+    @IBOutlet weak var ballView: UIView?
     var timer = Timer.scheduledTimer(withTimeInterval: 1/60, repeats: true) { _ in store.dispatch(NextFrame()) }
     var disposable: Disposable?
-    var ballView: UIView {
-        get {
-            guard let _ballView = _ballView else {
-                let _ballView = UIView(frame: CGRect(x: 0, y: 0, width: 2 * ballRadius, height: 2 * ballRadius))
-                _ballView.backgroundColor = .red
-                _ballView.layer.cornerRadius = ballRadius
-                canvasView?.addSubview(_ballView)
-                self._ballView = _ballView
-                return _ballView
-            }
-            return _ballView
-        }
-    }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        guard let canvasView = canvasView else { return }
+        guard let canvasView = canvasView, let ballView = ballView else { return }
         disposable = store.subscribe(self)
-        store.dispatch(Start(canvasSize: canvasView.bounds.size, ballRadius: ballRadius))
+        store.dispatch(Start(canvasSize: canvasView.bounds.size, ballRadius: ballView.frame.size.width / 2))
     }
     
     override func viewDidLayoutSubviews() {
@@ -49,12 +34,12 @@ class ViewController: UIViewController, SubscriberProtocol {
     
     func stateChanged(to newState: AppState?) {
         guard let state = newState else {
-            _ballView?.removeFromSuperview()
-            _ballView = nil
             jumpButton?.isEnabled = false
+            ballView?.isHidden = true
             return
         }
         jumpButton?.isEnabled = state.ballSpeed == nil
-        ballView.center = state.onFloor ? CGPoint(x: state.ballCenter.x, y: state.canvasSize.height - state.ballRadius) : state.ballCenter
+        ballView?.isHidden = false
+        ballView?.center = state.onFloor ? CGPoint(x: state.ballCenter.x, y: state.canvasSize.height - state.ballRadius) : state.ballCenter
     }
 }
