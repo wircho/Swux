827748e24c3b0d13a3fa1d37975ee7b62c32f1af
Adolfo Rodriguez <adolfo@mokriya.com>
2018-05-30 18:15:33 -0400

    Added Carthage support

---
+++ Framework/Swux/SupportingFiles/Swux.h
@@ -0,0 +1,19 @@
+//
+//  Swux.h
+//  Swux
+//
+//  Created by Adolfo Rodriguez on 2018-05-30.
+//  Copyright © 2018 Wircho. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Swux.
+FOUNDATION_EXPORT double SwuxVersionNumber;
+
+//! Project version string for Swux.
+FOUNDATION_EXPORT const unsigned char SwuxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swux/PublicHeader.h>
+
+
