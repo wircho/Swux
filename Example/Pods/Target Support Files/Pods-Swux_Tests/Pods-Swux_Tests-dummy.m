6ddf677337e863be1369b4b9cc81d14ecc69a93c
Adolfo Rodriguez <adolfo@mokriya.com>
2018-05-30 23:00:37 -0400

    Changed example to workspace with dependencies

--- Example/Pods/Target Support Files/Pods-Swux_Tests/Pods-Swux_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Swux_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_Swux_Tests
-@end
--- Example/Pods/Target Support Files/Swux/Swux-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Swux : NSObject
-@end
-@implementation PodsDummy_Swux
-@end
